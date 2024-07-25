232c0ff307923fdfcc570c4c5a97f32eaa967b02
AdiskaPutriAulia <22611145@students.uii.ac.id>
2024-07-25 23:40:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def preprocess_input(user_input):
     processed_input = pd.DataFrame(processed_input)
     processed_input[numeric_features] = scaler.transform(processed_input[numeric_features])
     return processed_input
+
 # CSS for styling
 st.markdown("""
     <style>
@@ -49,79 +50,4 @@ st.markdown("""
         background-color: #87CEEB;
     }
     h1 {
-        color: #4b4b4b;
-        text-align: center;
-        margin-bottom: 12px;
-    }
-    h3 {
-        color: #4b4b4b;
-    }
-    .stButton>button {
-        background-color: #4b4b4b;
-        color: black;
-        padding: 10px 24px;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-    }
-    .stButton>button:hover {
-        background-color: #4b4b4b;
-    }
-    .stNumberInput, .stSelectbox {
-        margin-bottom: 20px;
-    }
-    </style>
-""", unsafe_allow_html=True)
-
-# Antarmuka Streamlit
-st.title("Prediksi Output Customer Online Food")
-
-st.markdown("""
-    <style>
-    .main {
-        background-color: #FFC9DE;
-    }
-    </style>
-    <div class="centered-text">
-    <h3>Web ini akan memunculkan berapa kali seorang customer membeli Online Food</h3>
-       </div>
-""", unsafe_allow_html=True)
-
-# Input pengguna
-age = st.number_input('Age', min_value=18, max_value=100)
-gender = st.selectbox('Gender', ['Male', 'Female'])
-marital_status = st.selectbox('Marital Status', ['Single', 'Married'])
-occupation = st.selectbox('Occupation', ['Student', 'Employee', 'Self Employed'])
-monthly_income = st.selectbox('Monthly Income', ['No Income', 'Below Rs.10000', '10001 to 25000', '25001 to 50000', 'More than 50000'])
-educational_qualifications = st.selectbox('Educational Qualifications', ['Under Graduate', 'Graduate', 'Post Graduate'])
-family_size = st.number_input('Family size', min_value=1, max_value=20)
-latitude = st.number_input('Latitude', format="%f")
-longitude = st.number_input('Longitude', format="%f")
-pin_code = st.number_input('Pin code', min_value=100000, max_value=999999)
-
-user_input = {
-    'Age': age,
-    'Gender': gender,
-    'Marital Status': marital_status,
-    'Occupation': occupation,
-    'Monthly Income': monthly_income,
-    'Educational Qualifications': educational_qualifications,
-    'Family size': family_size,
-    'latitude': latitude,
-    'longitude': longitude,
-    'Pin code': pin_code
-}
-
-if st.button('Predict'):
-    user_input_processed = preprocess_input(user_input)
-    try:
-        prediction = model.predict(user_input_processed)
-        st.write(f'Prediction: {prediction[0]}')
-    except ValueError as e:
-        st.error(f"Error in prediction: {e}")
-
-# Tambahkan elemen HTML untuk output
-st.markdown("""
-    <h3>Output Prediksi</h3>
-    <p>Hasil prediksi akan ditampilkan di sini.</p>
-""", unsafe_allow_html=True)
+        color: #4b
